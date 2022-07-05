c1ee2841482d816621125ae0b303f9c287791499
Vitor Santos <vitor93gs@gmail.com>
2022-07-05 18:10:13 -0300

    ajuste de header fixo

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,25 +1,69 @@
 import styles from "./styles.module.css";
 import Image from "next/image";
 import dash from "../../assets/dash.svg";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 
 export function DropDown(props) {
   return (
-    <div className="h-28 w-dropdownHeader bg-themeWhite rounded-2xl p-3 flex">
-      <div className="bg-themeWhite">
-        <h1
-          className={`${styles.textH1} flex items-center text-themeOrange bg-themeWhite`}
-        >
-          Pratos
-        </h1>
-        <p
-          className={`${styles.textP} flex items-center text-xs bg-themeWhite`}
-        >
-          Aqui você pode ver ou adicionar novos itens na categoria selecionada.
-        </p>
-      </div>
-      <div className="bg-themeWhite flex items-center m-5">
-        <Image src={dash} alt="dash" className="bg-themeWhite" />
-      </div>
-    </div>
+    <Menu as="div" className="relative inline-block text-left z-50 rounded-2xl">
+      <Menu.Button className="h-28 w-dropdownHeader bg-themeWhite rounded-2xl p-3 flex">
+        <div className="bg-themeWhite">
+          <h1
+            className={`${styles.textH1} flex items-center text-themeOrange bg-themeWhite`}
+          >
+            Pratos
+          </h1>
+          <p
+            className={`${styles.textP} flex items-center text-xs bg-themeWhite`}
+          >
+            Aqui você pode ver ou adicionar novos itens na categoria
+            selecionada.
+          </p>
+        </div>
+        <div className="bg-themeWhite flex items-center m-5">
+          <Image src={dash} alt="dash" className="bg-themeWhite" />
+        </div>
+      </Menu.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-2xl shadow-lg bg-themeWhite ring-1 ring-black ring-opacity-5 focus:outline-none p-3">
+          <div className="py-1 flex flex-col bg-themeWhite ">
+            <Menu.Item>
+              <a
+                href="#"
+                className="bg-themeWhite hover:text-themeGray transition-all duration-500 ease-in-out"
+              >
+                Bebidas
+              </a>
+            </Menu.Item>
+            <Menu.Item>
+              <a
+                href="#"
+                className="bg-themeWhite hover:text-themeGray transition-all duration-500 ease-in-out"
+              >
+                Sobremesa
+              </a>
+            </Menu.Item>
+            <Menu.Item>
+              <a
+                href="#"
+                className="bg-themeWhite hover:text-themeGray transition-all duration-500 ease-in-out"
+              >
+                Pizzas
+              </a>
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
--- src/pages/produtos/index.js
+++ src/pages/produtos/index.js
@@ -7,26 +7,34 @@ export default function Pedidos() {
   return (
     <div className="flex">
       <NavBar />
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 ml-60 w-full place-items-center">
-        <div className="md:col-span-2 lg:col-span-3 2xl:col-span-4 3xl:col-span-5 flex justify-around w-full mt-4 mb-4">
+      <div>
+        <div className="fixed z-50 left-64 rounded-2xl">
           <DropDown />
-          <div>
-            <h1></h1>
-          </div>
         </div>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <AddButton />
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 ml-60 w-full place-items-center mt-40">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <AddButton />
+        </div>
       </div>
     </div>
   );
+}
+
+{
+  /* <div className="md:col-span-2 lg:col-span-3 2xl:col-span-4 3xl:col-span-5 flex justify-around w-full mt-4 mb-4">
+<DropDown />
+<div>
+  <h1></h1>
+</div>
+</div> */
 }
