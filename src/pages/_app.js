d537e73fb033b8c79a59f199f0846abbdaa2d6c4
Vitor Santos <vitor93gs@gmail.com>
2022-07-07 11:21:02 -0300

    adicionada logicade login para teste

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,24 @@
 import "../../styles/globals.css";
 
 import Head from "next/head";
+import { AuthContextComponent } from "../contexts/authContext";
 
-import { Provider }  from "react-redux";
+import { Provider } from "react-redux";
 import store from "../store";
-
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="font-theme">
-      <Head>
-        <link rel="stylesheet" href="/fonts/style.css" />
-      </Head>
-      
+    <AuthContextComponent>
+      <div className="font-theme">
+        <Head>
+          <link rel="stylesheet" href="/fonts/style.css" />
+        </Head>
 
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-
-
-    </div>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </div>
+    </AuthContextComponent>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,43 @@
 import styles from "../../styles/login.module.css";
+import { useRouter } from "next/router";
+import { useState, useContext } from "react";
+import { api } from "../api/index";
+import { AuthContext } from "../contexts/authContext";
 
 import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
+  const { setLoggedInUser } = useContext(AuthContext);
+
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+
+      const response = await api.post("/login", form);
+
+      setLoggedInUser(response.data);
+
+      localStorage.setItem("loggedInUser", JSON.stringify(response.data));
+
+      setLoading(false);
+
+      console.log("logado");
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="font-theme">
       <Head>
@@ -16,15 +50,27 @@ export default function Home() {
         <div className={styles.logo}></div>
       </div>
 
-      <div className={styles.inputContainer}>
-        <input className={styles.input} type="email" placeholder="Email" />
-        <input className={styles.input} type="password" placeholder="Senha" />
-        <Link href="../../produtos">
-          <a className={styles.button} type="submit">
-            Acessar
-          </a>
-        </Link>
-      </div>
+      <form className={styles.inputContainer} onSubmit={handleSubmit}>
+        <input
+          className={styles.input}
+          type="email"
+          placeholder="Email"
+          readOnly={loading}
+          required={true}
+          onChange={handleChange}
+        />
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="Senha"
+          readOnly={loading}
+          required={true}
+          onChange={handleChange}
+        />
+        <button type="submit" disabled={loading} className={styles.button}>
+          Acessar
+        </button>
+      </form>
 
       <div className={styles.textArea}>
         <Link href="#">
