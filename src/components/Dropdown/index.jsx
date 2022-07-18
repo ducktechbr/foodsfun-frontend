2dac4d6dc01c9603c353b1b7058ab207e80fe597
Vitor Santos <vitor93gs@gmail.com>
2022-07-18 09:52:58 -0300

    ajustado bug de card aparecendo sem categoria na pagina de produtos e lógica de informaçoes no dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,90 +1,97 @@
-import styles from "./styles.module.css";
-import Image from "next/image";
-import dash from "../../assets/dash.svg";
-import { Fragment, useEffect, useState } from "react";
-import { Menu, Transition } from "@headlessui/react";
-import { api } from "../../api/index";
-import categoryStore from "../../store/categoryStore";
+import styles from './styles.module.css';
+import Image from 'next/image';
+import dash from '../../assets/dash.svg';
+import { Fragment, useEffect, useState } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { api } from '../../api/index';
+import categoryStore from '../../store/categoryStore';
 
 export function DropDown() {
+	// cria novo estado de categoria
 
-  // cria novo estado de categoria
+	const [category, setCategory] = useState({
+		data: [{ title: '' }],
+	});
 
-  const [category, setCategory] = useState({
-    data: [{ title: "" }],
-  });
+	// função de buscar as categorias do usuário com a requisição da api
 
-// função de buscar as categorias do usuário com a requisição da api
+	async function getCategories() {
+		setCategory(await api.get('/getCategory'));
+	}
 
-  async function getCategories() {
-    setCategory(await api.get("/getCategory"));
-  }
+	// declaração dos estados do zustand de categoria selecionada e de set de categoria selecionada
 
-// declaração dos estados do zustand de categoria selecionada e de set de categoria selecionada
+	const selectedCategory = categoryStore((state) => state.selected);
+	const changeCategory = categoryStore((state) => state.changeCategory);
 
-  const selectedCategory = categoryStore((state) => state.selected);
-  const changeCategory = categoryStore((state) => state.changeCategory);
+	// função que muda no store do zustand o nome da categoria e seu ID
 
-  // função que muda no store do zustand o nome da categoria e seu ID
+	function handleSelectedCategory(target, targetId) {
+		changeCategory(target, targetId);
+	}
 
-  function handleSelectedCategory(target, targetId) {
-    changeCategory(target, targetId);
-  }
+	useEffect(() => {
+		getCategories();
+		category.data[0]
+			? changeCategory(category.data[0].title, category.data[0].id)
+			: null;
+	}, []);
 
+	useEffect(() => {
+		category.data[0]
+			? changeCategory(category.data[0].title, category.data[0].id)
+			: null;
+	}, [category]);
 
-  useEffect(() => {
-    getCategories();
-    changeCategory(category.data[0].title, category.data[0].id);
-  }, []);
+	return (
+		<Menu as="div" className="relative inline-block text-left rounded-2xl">
+			<Menu.Button className="  bg-themeWhite rounded-2xl p-3 flex">
+				<div className="bg-themeWhite">
+					<h1
+						className={`${styles.textH1} flex items-center text-themeOrange bg-themeWhite`}
+					>
+						{selectedCategory}
+					</h1>
+				</div>
+				<div className="bg-themeWhite flex items-center ml-5 mt-2">
+					<Image src={dash} alt="dash" className="bg-themeWhite" />
+				</div>
+			</Menu.Button>
 
-  useEffect(() => {
-    changeCategory(category.data[0].title, category.data[0].id);
-  }, [category]);
-
-  return (
-    <Menu as="div" className="relative inline-block text-left rounded-2xl">
-      <Menu.Button className="  bg-themeWhite rounded-2xl p-3 flex">
-        <div className="bg-themeWhite">
-          <h1
-            className={`${styles.textH1} flex items-center text-themeOrange bg-themeWhite`}
-          >
-            {selectedCategory}
-          </h1>
-        </div>
-        <div className="bg-themeWhite flex items-center ml-5 mt-2">
-          <Image src={dash} alt="dash" className="bg-themeWhite" />
-        </div>
-      </Menu.Button>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-2xl shadow-lg bg-themeWhite ring-1 ring-black ring-opacity-5 focus:outline-none p-3 z-20">
-          <div className="py-1 flex flex-col bg-themeWhite ">
-            {category.data[0].title !== ""
-              ? category.data.map((current, key) => {
-                  return (
-                    <button
-                      key={key}
-                      onClick={() =>
-                        handleSelectedCategory(current.title, current.id)
-                      }
-                      className="bg-themeWhite hover:text-themeGray transition-all duration-500 ease-in-out"
-                    >
-                      <Menu.Item as="div">{current.title}</Menu.Item>
-                    </button>
-                  );
-                })
-              : null}
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
-  );
+			<Transition
+				as={Fragment}
+				enter="transition ease-out duration-100"
+				enterFrom="transform opacity-0 scale-95"
+				enterTo="transform opacity-100 scale-100"
+				leave="transition ease-in duration-75"
+				leaveFrom="transform opacity-100 scale-100"
+				leaveTo="transform opacity-0 scale-95"
+			>
+				<Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-2xl shadow-lg bg-themeWhite ring-1 ring-black ring-opacity-5 focus:outline-none p-3 z-20">
+					<div className="py-1 flex flex-col bg-themeWhite ">
+						{category.data[0]
+							? category.data.map((current, key) => {
+									return (
+										<button
+											key={key}
+											onClick={() =>
+												handleSelectedCategory(
+													current.title,
+													current.id
+												)
+											}
+											className="bg-themeWhite hover:text-themeGray transition-all duration-500 ease-in-out"
+										>
+											<Menu.Item as="div">
+												{current.title}
+											</Menu.Item>
+										</button>
+									);
+							  })
+							: null}
+					</div>
+				</Menu.Items>
+			</Transition>
+		</Menu>
+	);
 }
--- src/pages/produtos/index.js
+++ src/pages/produtos/index.js
@@ -1,86 +1,89 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-import { NavBar } from "../../components/NavBar";
-import { DropDown } from "../../components/Dropdown";
-import { BackgroundBanner } from "../../components/BackgroundBanner";
-import Card from "../../components/Card";
-import AddButton from "../../components/AddButton";
-import { ProtectedRoute } from "../../middlewares/protectedRoute";
-import categoryStore from "../../store/categoryStore";
-import reloadStore from "../../store/reloadStore";
-import { api } from "../../api";
-import styles from "./styles.module.css";
-import { useEffect, useState } from "react";
+import { NavBar } from '../../components/NavBar';
+import { DropDown } from '../../components/Dropdown';
+import { BackgroundBanner } from '../../components/BackgroundBanner';
+import Card from '../../components/Card';
+import AddButton from '../../components/AddButton';
+import { ProtectedRoute } from '../../middlewares/protectedRoute';
+import categoryStore from '../../store/categoryStore';
+import reloadStore from '../../store/reloadStore';
+import { api } from '../../api';
+import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
 
 function Page() {
-  // criação do estado da lista de produtos, é definida como estado inicial data[0].title = "" para não dar erro de primeiro render no return
+	// criação do estado da lista de produtos, é definida como estado inicial data[0].title = "" para não dar erro de primeiro render no return
 
-  const [products, setProducts] = useState({ data: [{ title: "" }] });
+	const [products, setProducts] = useState({ data: [{ title: '' }] });
 
-  // busca a categoria selecionada no store do zustand
+	// busca a categoria selecionada no store do zustand
 
-  const selectedCategory = categoryStore((state) => state.selected);
-  const reloadState = reloadStore((state) => state.reload);
+	const selectedCategory = categoryStore((state) => state.selected);
+	const reloadState = reloadStore((state) => state.reload);
 
-  // função pega o array de produtos ligados à categoria selecionada
+	// função pega o array de produtos ligados à categoria selecionada
 
-  async function getProducts() {
-    selectedCategory !== ""
-      ? setProducts(await api.get(`/getProducts/${selectedCategory}`))
-      : null;
-  }
+	async function getProducts() {
+		selectedCategory !== ''
+			? setProducts(await api.get(`/getProducts/${selectedCategory}`))
+			: null;
+	}
 
-  // renderiza a página toda vez que a categoria selecionada é atualizada no zustand
+	// renderiza a página toda vez que a categoria selecionada é atualizada no zustand
 
-  useEffect(() => {
-    getProducts();
-  }, [selectedCategory]);
+	useEffect(() => {
+		getProducts();
+	}, [selectedCategory]);
 
-  useEffect(() => {
-    getProducts();
-  }, [reloadState]);
+	useEffect(() => {
+		getProducts();
+	}, [reloadState]);
 
-  return (
-    <div className="flex">
-      <Head>
-        <title>FoodsFun - Produtos</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+	return (
+		<div className="flex">
+			<Head>
+				<title>FoodsFun - Produtos</title>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+			</Head>
 
-      <div className="w-60">
-        <NavBar />
-      </div>
+			<div className="w-60">
+				<NavBar />
+			</div>
 
-      <div className={styles.screen}>
-        <BackgroundBanner />
+			<div className={styles.screen}>
+				<BackgroundBanner />
 
-        <div className="w-full rounded-2xl flex justify-between items-center">
-          <div className="h-24 mt-5 px-5 flex w-full justify-between items-center">
-            <DropDown />
-            <AddButton />
-          </div>
-        </div>
+				<div className="w-full rounded-2xl flex justify-between items-center">
+					<div className="h-24 mt-5 px-5 flex w-full justify-between items-center">
+						<DropDown />
+						<AddButton />
+					</div>
+				</div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 4xl:grid-cols-5  w-full place-items-center mt-14">
-          {products.data[0]
-            ? products.data.map((cur, key) => {
-                return (
-                  <Card
-                    product = {cur}
-                    title={cur.title}
-                    category={selectedCategory}
-                    key={key}
-                    price={cur.price}
-                    id={cur.id}
-                  />
-                );
-              })
-            : null}
-        </div>
-      </div>
-    </div>
-  );
+				<div className="grid md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 4xl:grid-cols-5  w-full place-items-center mt-14">
+					{products.data[0].title !== ''
+						? products.data.map((cur, key) => {
+								return (
+									<Card
+										product={cur}
+										title={cur.title}
+										category={selectedCategory}
+										key={key}
+										price={cur.price}
+										id={cur.id}
+									/>
+								);
+						  })
+						: null}
+				</div>
+			</div>
+		</div>
+	);
 }
 export default function Produtos() {
-  return <ProtectedRoute component={Page} />;
+	return <ProtectedRoute component={Page} />;
 }
