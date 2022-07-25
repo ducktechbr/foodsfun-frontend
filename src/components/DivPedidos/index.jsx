05936835b7caf9b880c51acf7b2293dfbd491557
Vitor Santos <vitor93gs@gmail.com>
2022-07-25 15:32:03 -0300

    adicionada logica de numero de pedido

--- src/components/DivPedidos/index.jsx
+++ src/components/DivPedidos/index.jsx
@@ -1,75 +1,90 @@
-import { useEffect, useState } from "react";
-import ModalInfo from "../ModalInfo";
-import { BsFillChatLeftTextFill } from "react-icons/bs";
-import { api } from "../../api";
+import { useEffect, useState } from 'react';
+import ModalInfo from '../ModalInfo';
+import { BsFillChatLeftTextFill } from 'react-icons/bs';
+import { api } from '../../api';
 
 export default function DivPedidos(props) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [tableNumber, setTableNumber] = useState(0);
-  const [table, setTable] = useState([]);
+	const [isOpen, setIsOpen] = useState(false);
+	const [tableNumber, setTableNumber] = useState(0);
+	const [table, setTable] = useState([]);
+	const [number, setNumber] = useState('');
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+	function closeModal() {
+		setIsOpen(false);
+	}
 
-  function openModal() {
-    setIsOpen(true);
-  }
+	function openModal() {
+		setIsOpen(true);
+	}
 
-  async function getTableNumber() {
-    setTable(await api.get(`/getTables`));
-  }
+	async function getTableNumber() {
+		setTable(await api.get(`/getTables`));
+	}
 
-  useEffect(() => {
-    getTableNumber();
-  }, []);
+	useEffect(() => {
+		getTableNumber();
+	}, []);
 
-  useEffect(() => {
-    if (table.data) {
-      const tableNums = table.data.filter(
-        (current) => current.id === props.current.tableId
-      );
-      tableNums[0] ? setTableNumber(tableNums[0].number) : null;
-    }
-  }, [table]);
+	useEffect(() => {
+		if (table.data) {
+			const tableNums = table.data.filter(
+				(current) => current.id === props.current.tableId
+			);
+			tableNums[0] ? setTableNumber(tableNums[0].number) : null;
+		}
+		setNumber(String(props.current.number).slice(-3));
+	}, [table]);
 
-  return (
-    <div className="flex px-2 h-14 bg-white" key={props.key}>
-      <div className="w-1/8 bg-white">
-        <span className="p-2 rounded-2xl bg-white border ">123123</span>
-      </div>
-      <div className="w-1/8 bg-white">
-        <span className="p-2 rounded-2xl bg-white border ">{tableNumber}</span>
-      </div>
-      <div className="w-1/8 bg-white">
-        <span className="p-2 rounded-2xl bg-white border ">
-          {props.current.quantity}
-        </span>
-      </div>
-      <div className="w-4/8 bg-white flex">
-        <span className="rounded-2xl bg-white ">{props.current.title}</span>
+	async function handleToggle() {
+		// await api.patch(`/toggleOrder`);
+	}
 
-        <button type="button" onClick={openModal}>
-          <BsFillChatLeftTextFill
-            size={16}
-            color="#AAA"
-            style={{
-              backgroundColor: "transparent",
-              marginLeft: "10px",
-            }}
-          />
-        </button>
-      </div>
-      <div className="w-1/8 bg-white">
-        <span className="p-2 rounded-2xl bg-green-400 border border-green-400">
-          {props.current.active ? "Preparando" : "Concluido"}
-        </span>
-      </div>
-      <ModalInfo
-        isOpen={isOpen}
-        closeModal={closeModal}
-        info={props.current.info}
-      />
-    </div>
-  );
+	return (
+		<div className="flex px-2 h-14 bg-white" key={props.key}>
+			<div className="w-1/8 bg-white">
+				<span className="p-2 rounded-2xl bg-white border ">
+					{number}
+				</span>
+			</div>
+			<div className="w-1/8 bg-white">
+				<span className="p-2 rounded-2xl bg-white border ">
+					{tableNumber}
+				</span>
+			</div>
+			<div className="w-1/8 bg-white">
+				<span className="p-2 rounded-2xl bg-white border ">
+					{props.current.quantity}
+				</span>
+			</div>
+			<div className="w-4/8 bg-white flex">
+				<span className="rounded-2xl bg-white ">
+					{props.current.title}
+				</span>
+
+				<button type="button" onClick={openModal}>
+					<BsFillChatLeftTextFill
+						size={16}
+						color="#AAA"
+						style={{
+							backgroundColor: 'transparent',
+							marginLeft: '10px',
+						}}
+					/>
+				</button>
+			</div>
+			<div className="w-1/8 bg-white">
+				<button
+					onClick={handleToggle}
+					className="p-2 rounded-2xl bg-green-400 border border-green-400"
+				>
+					{props.current.active ? 'Preparando' : 'Concluido'}
+				</button>
+			</div>
+			<ModalInfo
+				isOpen={isOpen}
+				closeModal={closeModal}
+				info={props.current.info}
+			/>
+		</div>
+	);
 }
--- src/pages/pedidos/index.js
+++ src/pages/pedidos/index.js
@@ -1,97 +1,118 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-import { IoIosArrowDropright } from "react-icons/io";
+import { IoIosArrowDropright } from 'react-icons/io';
 
+import NavBar from '../../components/NavBar';
+import { BackgroundBanner } from '../../components/BackgroundBanner';
+import AddOrder from '../../components/AddOrder';
+import { api } from '../../api';
 
-import NavBar from "../../components/NavBar";
-import { BackgroundBanner } from "../../components/BackgroundBanner";
-import AddOrder from "../../components/AddOrder";
-import { api } from "../../api";
+import { ProtectedRoute } from '../../middlewares/protectedRoute';
 
-import { ProtectedRoute } from "../../middlewares/protectedRoute";
-
-import styles from "./styles.module.css";
-import { useEffect, useMemo, useState } from "react";
-import ModalInfo from "../../components/ModalInfo";
-import DivPedidos from "../../components/DivPedidos";
+import styles from './styles.module.css';
+import { useEffect, useMemo, useState } from 'react';
+import ModalInfo from '../../components/ModalInfo';
+import DivPedidos from '../../components/DivPedidos';
 
 function Page() {
-  const [pedidos, setPedidos] = useState({ data: [{ title: "" }] });
+	const [pedidos, setPedidos] = useState({ data: [{ title: '' }] });
+	const [pedidosSemOrdem, setPedidosSemOrdem] = useState({
+		data: [{ title: '' }],
+	});
 
-  // const [filtroMesa , setFiltroMesa] = useState(2)
+	useEffect(() => {
+		getOrders();
+	}, []);
 
-  useEffect(() => {
-    getOrders();
-  }, []);
+	useEffect(() => {
+		var arrayPedidos = [];
+		arrayPedidos = pedidosSemOrdem.data;
 
-  async function getOrders() {
-    setPedidos(await api.get(`/getOrders`));
-  }
+		setPedidos({
+			data: arrayPedidos.sort((a, b) =>
+				a.number < b.number ? 1 : b.number < a.number ? -1 : 0
+			),
+		});
+	}, [pedidosSemOrdem]);
 
-  return (
-    <div className="flex">
-      <Head>
-        <title>FoodsFun - Pedidos</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <div className="w-60"></div>
-      <NavBar />
+	async function getOrders() {
+		setPedidosSemOrdem(await api.get(`/getOrders`));
+	}
 
-      <div className={styles.screen}>
-        <BackgroundBanner />
+	return (
+		<div className="flex">
+			<Head>
+				<title>FoodsFun - Pedidos</title>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+			</Head>
+			<div className="w-60"></div>
+			<NavBar />
 
-        <div className="mt-5 px-5 flex justify-end">
-          <AddOrder />
-        </div>
-        <div className="mt-14 mx-4 h-48 border-2 rounded-2xl bg-white ">
-          <div className="space-y-0  h-16 bg-themeOrange text-white text-[1.3rem] py-2 px-2 rounded-t-lg">
-            <div className="flex justify-around mt-2 bg-transparent">
-              <div className="w-1/8 bg-transparent">Pedido</div>
-              <div className="w-1/8 bg-transparent flex">
-                Mesa
-                <button>
-                  <IoIosArrowDropright
-                    size={25}
-                    color="#fff"
-                    style={{
-                      backgroundColor: "transparent",
-                      marginLeft: "2px",
-                    }}
-                  />
-                </button>
-              </div>
+			<div className={styles.screen}>
+				<BackgroundBanner />
 
-              <div className="w-1/8 bg-transparent">Quantidade</div>
-              <div className="w-4/8 bg-transparent">Produto</div>
-              <div className="w-1/8 bg-transparent flex">
-                Status
-                <button type="button">
-                  <IoIosArrowDropright
-                    size={25}
-                    color="#fff"
-                    style={{
-                      backgroundColor: "transparent",
-                      marginLeft: "2px",
-                    }}
-                  />
-                </button>
-              </div>
-            </div>
-            {/* as proximas divs serao geradas automaticamente */}
-          </div>
-          <div className="mt-5">
-            {pedidos.data[0]
-              ? pedidos.data.map((current, key) => {
-                  return <DivPedidos current={current} key={key} />;
-                })
-              : null}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="mt-5 px-5 flex justify-end">
+					<AddOrder />
+				</div>
+				<div className="mt-14 mx-4 h-48 border-2 rounded-2xl bg-white ">
+					<div className="space-y-0  h-16 bg-themeOrange text-white text-[1.3rem] py-2 px-2 rounded-t-lg">
+						<div className="flex justify-around mt-2 bg-transparent">
+							<div className="w-1/8 bg-transparent">Pedido</div>
+							<div className="w-1/8 bg-transparent flex">
+								Mesa
+								<button>
+									<IoIosArrowDropright
+										size={25}
+										color="#fff"
+										style={{
+											backgroundColor: 'transparent',
+											marginLeft: '2px',
+										}}
+									/>
+								</button>
+							</div>
+
+							<div className="w-1/8 bg-transparent">
+								Quantidade
+							</div>
+							<div className="w-4/8 bg-transparent">Produto</div>
+							<div className="w-1/8 bg-transparent flex">
+								Status
+								<button type="button">
+									<IoIosArrowDropright
+										size={25}
+										color="#fff"
+										style={{
+											backgroundColor: 'transparent',
+											marginLeft: '2px',
+										}}
+									/>
+								</button>
+							</div>
+						</div>
+						{/* as proximas divs serao geradas automaticamente */}
+					</div>
+					<div className="mt-5">
+						{pedidos.data[0]
+							? pedidos.data.map((current, key) => {
+									return (
+										<DivPedidos
+											current={current}
+											key={key}
+										/>
+									);
+							  })
+							: null}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default function Pedidos() {
-  return <ProtectedRoute component={Page} />;
+	return <ProtectedRoute component={Page} />;
 }
