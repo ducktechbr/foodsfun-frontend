db33477a857bbba4d0cac55b47a93b7c01af21f1
Vitor Santos <vitor93gs@gmail.com>
2022-08-02 16:30:10 -0300

    pre merge com higor

--- src/components/CardProdutosApp/index.jsx
+++ src/components/CardProdutosApp/index.jsx
@@ -1,18 +1,19 @@
 import styles from "./styles.module.scss";
 import Image from "next/image";
 
-
-
 export default function CardProdutos(props) {
-
-  const {image, title, price, description} = props.card
+  const { image, title, price, description } = props.card;
   // const decoded = image.content.toString().replace("data:image/png;base64, " || "image/jpeg;base64" || "image/svg;base64" || "image/gif;base64" , "");
 
   return (
-    <div className="mb-3 border flex flex-col justify-between">
-      
-        <Image src={image} width="120px" height="120px" alt="Imagem do produto" />
-      
+    <div className="mb-3 flex flex-col justify-between">
+      <Image
+        src={image}
+        width="120px"
+        height="120px"
+        alt="Imagem do produto"
+        className="bg-transparent"
+      />
 
       <div className={styles.container}>
         <div className={styles.namePrice}>
@@ -20,9 +21,7 @@ export default function CardProdutos(props) {
           <h1>{price}</h1>
         </div>
 
-        <p className={styles.text}>
-          {description}
-        </p>
+        <p className={styles.text}>{description}</p>
       </div>
     </div>
   );
